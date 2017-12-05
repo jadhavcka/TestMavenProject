17deaf3859026a2df77417adddb95864fc3810a1
Kirtesh Wani <wanikirtesh@gmail.com>
2017-12-05 18:23:54 +0530

    Initial Commit

---
+++ src/test/java/com/sbn/test/MySimpleTest.java
@@ -0,0 +1,16 @@
+package com.sbn.test;
+
+import org.testng.annotations.Test;
+
+public class MySimpleTest {
+
+    @Test
+    public void myTest1(){
+        System.out.println("Running the test 1");
+    }
+
+    @Test
+    public void myTest2(){
+        System.out.println("Running the test 2");
+    }
+}
