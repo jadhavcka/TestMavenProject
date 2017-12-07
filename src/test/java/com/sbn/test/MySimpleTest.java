48ecfcb58da317366dfde7e72ee861ccc57af450
Kirtesh Wani <wanikirtesh@gmail.com>
2017-12-07 10:52:25 +0530

    added tests for remotewebdriver

--- src/test/java/com/sbn/test/MySimpleTest.java
+++ src/test/java/com/sbn/test/MySimpleTest.java
@@ -1,16 +1,28 @@
 package com.sbn.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-public class MySimpleTest {
+import java.net.MalformedURLException;
+import java.net.URL;
 
+public class MySimpleTest {
+    String hubURL = "";
     @Test
-    public void myTest1(){
-        System.out.println("Running the test 1");
+    public void myTest1() throws MalformedURLException {
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL(hubURL),capabilities);
+        driver.get("http://www.google.com");
+        driver.quit();
     }
 
     @Test
-    public void myTest2(){
-        System.out.println("Running the test 2");
+    public void myTest2() throws MalformedURLException {
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL(hubURL),capabilities);
+        driver.get("http://www.bing.com");
+        driver.quit();
     }
 }
